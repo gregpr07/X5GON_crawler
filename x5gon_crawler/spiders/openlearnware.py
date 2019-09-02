dfae97c1842c23527b18042fa9172aefea85a737
gregpr07 <gregor.ninjaboy@gmail.com>
2019-09-02 12:56:59 +0200

    try_except

--- x5gon_crawler/spiders/openlearnware.py
+++ x5gon_crawler/spiders/openlearnware.py
@@ -83,89 +83,94 @@ with open(OUTPUT_LOCATION+'openlearnware.json', 'w+', encoding='utf-8') as f:
         r_json = requests.get(url).json()
         content = {}
         if r_json and not r_json['deleted'] and r_json['open']:
+            try:
+                # if material has a parent
+                if r_json['parent']:
 
-            # if material has a parent
-            if r_json['parent']:
+                    for uuid_loop in parent_meta:
+                        # and material uuid equals any meta that is saved
+                        if r_json['parent'] == uuid_loop:
+                            uuid = r_json['uuid']
 
-                for uuid_loop in parent_meta:
-                    # and material uuid equals any meta that is saved
-                    if r_json['parent'] == uuid_loop:
-                        uuid = r_json['uuid']
+                            print('found parent for', uuid)
 
-                        print('found parent for', uuid)
+                            content = parent_meta[uuid_loop]
 
-                        content = parent_meta[uuid_loop]
+                            material_url = getMaterialLink(
+                                r_json['uuid'], r_json['characteristicType'])
 
-                        material_url = getMaterialLink(
-                            r_json['uuid'], r_json['characteristicType'])
+                            typez = getType(r_json['characteristicType'])
 
-                        typez = getType(r_json['characteristicType'])
+                            content['type'] = typez
+                            content['material_url'] = material_url
 
-                        content['type'] = typez
-                        content['material_url'] = material_url
+                            content['metadata']['id'] = materialID
+                            content['metadata']['uuid'] = uuid
+                            content['metadata']['if_child_meta']['parent_uuid'] = r_json['parent']
+                            content['metadata']['if_child_meta']['title'] = r_json['name']
 
-                        content['metadata']['id'] = materialID
-                        content['metadata']['uuid'] = uuid
-                        content['metadata']['if_child_meta']['parent_uuid'] = r_json['parent']
-                        content['metadata']['if_child_meta']['title'] = r_json['name']
+                            #parent_meta.pop(uuid_loop, None)
 
-                        #parent_meta.pop(uuid_loop, None)
+                        else:
+                            continue
+                # else means that material is a parent
+                else:
+                    title = r_json['name']
+                    uuid = r_json['uuid']
 
-                    else:
-                        continue
-            # else means that material is a parent
-            else:
-                title = r_json['name']
-                uuid = r_json['uuid']
+                    material_url = getMaterialLink(
+                        r_json['uuid'], r_json['characteristicType'])
+                    material_uri = resourceURL + \
+                        slugify(title)+'-'+str(materialID)
 
-                material_url = getMaterialLink(
-                    r_json['uuid'], r_json['characteristicType'])
-                material_uri = resourceURL+slugify(title)+'-'+str(materialID)
+                    language = languages[int(r_json['languages'][0]['id'])]
 
-                language = languages[int(r_json['languages'][0]['id'])]
+                    licensecode = r_json['code']
+                    licenca = f'https://creativecommons.org/licenses/{licensecode}/3.0/'
+                    # resource.tpl.html - says that there are only 3.0 licences
 
-                licensecode = r_json['code']
-                licenca = f'https://creativecommons.org/licenses/{licensecode}/3.0/'
-                # resource.tpl.html - says that there are only 3.0 licences
+                    date_created = convertTime(r_json["creationDate"])
+                    date_retrieved = dateYMD
 
-                date_created = convertTime(r_json["creationDate"])
-                date_retrieved = dateYMD
+                    description = r_json['description']
 
-                description = r_json['description']
+                    areas = r_json['areas']
 
-                areas = r_json['areas']
+                    typez = getType(r_json['characteristicType'])
 
-                typez = getType(r_json['characteristicType'])
+                    content = {
+                        'title': title,
+                        'description': description,
+                        'provider_uri': material_uri,
+                        'material_url': material_url,
+                        'language': language,
+                        'type': typez,
+                        'date_created': date_created,
+                        'date_retrieved': date_retrieved,
+                        'license': licenca,
+                        'metadata': {
+                            'id': materialID,
+                            'uuid': uuid,
+                            'areas': areas,
+                            'collections': r_json["collections"],
+                            'if_child_meta': {
+                                'parent_uuid': '',
+                                'title': ''
+                            }
 
-                content = {
-                    'title': title,
-                    'description': description,
-                    'provider_uri': material_uri,
-                    'material_url': material_url,
-                    'language': language,
-                    'type': typez,
-                    'date_created': date_created,
-                    'date_retrieved': date_retrieved,
-                    'license': licenca,
-                    'metadata': {
-                        'id': materialID,
-                        'uuid': uuid,
-                        'areas': areas,
-                        'collections': r_json["collections"],
-                        'if_child_meta': {
-                            'parent_uuid': '',
-                            'title': ''
                         }
-
                     }
-                }
 
-                if r_json['childs']:
-                    parent_meta[uuid] = content
+                    if r_json['childs']:
+                        parent_meta[uuid] = content
 
-            # ouput
-            json.dump(content, f, indent=2)
-            print('appended to json')
-            f.write(',\n')
+                # ouput
+                json.dump(content, f, indent=2)
+                print('appended to json')
+                f.write(',\n')
+            except Exception as e:
+                print('\033[93mEXCEPTION:', e)
+                print('\033[92m...continuing with the loop\033[0m\n')
+                continue
 
     f.write('\n]')
